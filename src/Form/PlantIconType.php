9b4ecc0f330cfb741b58928f090bf98b9b98a0a3
Sarah Roupec <roupecs@yahoo.com>
2020-04-20 18:49:28 +0200

    image update/ rita redirect update

--- src/Form/PlantIconType.php
+++ src/Form/PlantIconType.php
@@ -6,6 +6,9 @@ use App\Entity\Plant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class PlantIconType extends AbstractType
 {
