9f3e849d2838c2da20b9ff405283606c503a9006
Isa <S1810238002@students.fh-hagenberg.at>
2020-04-23 19:58:03 +0200

    plantController (started, not finished)

---
+++ src/Controller/PlantController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Plant;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class PlantController extends AbstractController {
+    /**
+     * @Route("/plant", name="plant")
+     */
+    public function plant()
+    {
+
+        return $this->render('searchPlant.html.twig', [
+            'controller_name' => 'PlantController'
+        ]);
+    }
+
+
+    /**
+     * @Route("/plant/{name}", name="plant")
+     */
+    public function index($name)
+    {
+        $plant = $this->getDoctrine()->getRepository(Plant::class)->find($name);
+
+        if (!$plant) {
+            throw $this->createNotFoundException(
+                'No plant found for plant ' . $plant
+            );
+        }
+
+        return $this->render('plant.html.twig', [
+            'controller_name' => 'PlantController', 'plant' => $plant
+        ]);
+    }
+
+    /**
+     * @Route("/plant/{name}", name="plantName")
+     */
+    /*public function index($name)
+    {
+        $plant = $this->getDoctrine()
+            ->getRepository(Plant::class)
+            ->find($name);
+
+        if (!$plant) {
+            throw $this->createNotFoundException(
+                'No plant found for name ' . $name
+            );
+        }
+
+        return $this->render('plant.html.twig', [
+            'controller_name' => 'PlantController', 'plant' => $plant
+        ]);
+    }*/
+}
