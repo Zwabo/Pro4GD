a3a96ca49c5f3c2dc3c7dcc494004bf0645b1b69
zwabo <sebastian.winkler@students.fh-hagenberg.at>
2020-04-27 15:02:54 +0200

    Implementation of Friend-System, first iteration of Friend-Page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
 
-    /**
+/**
  * @Route("/profile", name="profile")
  */
-    public function index()
+    public function profile()
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -20,6 +20,34 @@ class UserController extends AbstractController
 
         return $this->render('profile.html.twig', [
             'controller_name' => 'UserController', 'user' => $user
+        ]);
+    }
+    /**
+     * @Route("/profile/friends", name="profile_friends")
+     */
+    public function friends()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        /* @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        $friends = array();
+        foreach($user->getOutgoingFriendRequests() as $request){
+            if($request->getConfirmed()){
+                array_push($friends, $request->getReceiver());
+            }
+        }
+        foreach($user->getIncomingFriendRequests() as $request){
+            if($request->getConfirmed()){
+                array_push($friends, $request->getSender());
+            }
+        }
+
+        return $this->render('friends.html.twig', [
+            'controller_name' => 'UserController',
+            'user' => $user,
+            'friends' => $friends
         ]);
     }
 }
