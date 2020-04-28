7bcefbf1dd4af86c85d312b3ae75a6b7fff7234b
rihai <rhainzl@hotmail.com>
2020-04-28 17:20:15 +0200

    Made the relation between Userplant and User work

--- src/DataFixtures/NewsFixture.php
+++ src/DataFixtures/NewsFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\News;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,7 +12,10 @@ class NewsFixture extends Fixture
     {
         $news = new News();
 
-        $news->setTitle();
+        $news->setTitle("TestTitle1: Wie ich meine Pflanzen nicht umbringe!");
+        $news->setDatePosted(date_create('2019-05-19 12:30:03'));
+        $news->setShortText("I guess this is the short version of the article");
+        $news->setLongText("this is the very long article everyone was looking forward this whole time and it is even getting longer by the second.");
 
 
 
