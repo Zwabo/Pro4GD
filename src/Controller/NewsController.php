ecac410999ba8a2feb689bb74ef1331281934e72
rihai <rhainzl@hotmail.com>
2020-04-30 14:44:52 +0200

    news site added

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -10,7 +10,7 @@ class NewsController extends AbstractController
     /**
      * @Route("/news", name="news")
      */
-    
+
     public function index()
     {
         return $this->render('news/index.html.twig', [
