2cd681b4963c4500085e0efd2ede2162d94c4054
rihai <rhainzl@outlook.com>
2020-06-07 20:41:05 +0200

    settings added

---
+++ assets/js/components/pages/Settings.vue
@@ -0,0 +1,150 @@
+<template>
+
+    <div id="root">
+        <div v-if="profileUser != null">
+
+            <div class="container-fluid">
+
+                <!-- upper profile part -->
+                <div class="row bgDarkGrey fontWhite" id="userInfoCnt">
+                    <div class="col-lg-4" id="userPicCnt">
+                        <img class="userPicture" v-bind:src="profileUser.userPic">
+                    </div>
+
+                    <div id="userCnt" class="col-lg-5">
+                        <h1>{{ profileUser.firstname}} {{ profileUser.lastname }}</h1>
+                        <h2> {{ profileUser.username }}</h2>
+                        <p> Rang: {{ profileUser.level }}</p>
+                        <p>Sprössling</p>           <!-- Rang Benennung: aus Lvl berechnet -->
+                    </div>
+
+                    </div>
+                </div>
+
+                <!--lower profile part-->
+                <div class="row marginLeftRight">
+                    <div class="col-lg-12">
+                        <div class="left">
+                            <div id="settingsTitle">
+                                <h3 id="settings"class="h3Margin">Einstellungen </h3>
+                                <div class="greenLine"></div>
+
+                            </div>
+
+                            <div>
+                                <h3 id="push"class="h3Margin">Push-Benachrichtigungen</h3>
+                                <input id="toggle0"type="checkbox" checked data-toggle="toggle" data-style="ios">
+
+                                <div class="greenLine"></div>
+                                <div>
+                                    <h4 class="bulletPoints">Gieß-Erinnerungen</h4>
+                                    <p class="bulletExplanation">Erlauben für die Gieß-Erinnerungen E-Mails und Push-Up Nachrichten an ihren Account zu schicken.</p>
+
+                                    <input id="toggle1"type="checkbox" checked data-toggle="toggle" data-style="ios">
+
+                                    <h4 class="bulletPoints">Tipps-Benachrichtigungen</h4>
+                                    <p class="bulletExplanation">Erlauben für die Kategorie Tipps und Tricks E-Mails und Push-Up Nachrichten an ihren Account zu schicken.</p>
+                                    <input id="toggle2"type="checkbox" checked data-toggle="toggle" data-style="ios">
+
+                                    <h4 class="bulletPoints">Tägliche News</h4>
+                                    <p class="bulletExplanation">Erlauben für die Kategorie Tägliche News E-Mails und Push-Up Nachrichten an ihren Account zu schicken.</p>
+                                    <input id="toggle3"type="checkbox" checked data-toggle="toggle" data-style="ios">
+                                </div>
+                                    </div>
+
+                            <div>
+                                <h3 id="privateHeader"class="h3Margin">Privatsphäreeinstellungen</h3>
+
+
+                                <div class="greenLine"></div>
+                                <div>
+                                    <h4 class="col-lg-4">Userprofil</h4>
+                                    <div class ="col-lg-8">
+                                    <input class="radioButton" name="userprofile"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="userprofile"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="userprofile"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    </div>
+                                    <div class="greenLine"></div>
+
+                                   <h4 class="private">Geburtsdatum</h4>
+                                    <input class="radioButton" name="birthday"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="birthday"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="birthday"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+
+                                    <h4 class="private">Online-Status</h4>
+
+                                    <input class="radioButton" name="onlinestatus"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="onlinestatus"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="onlinestatus"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+
+                                    <h4 class="private">Errungenschaften</h4>
+                                    <input class="radioButton" name="achievements"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="achievements"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="achievements"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+
+                                    <h4 class="private">Besuchernachrichten</h4>
+                                    <input class="radioButton" name="messages"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="messages"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="messages"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+
+                                    <h4 class="private">Freunde</h4>
+                                    <input class="radioButton" name="friendlist"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="friendlist"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="friendlist"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+
+                                    <h4 class="private">Garten</h4>
+                                    <input class="radioButton" name="garden"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="garden"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="garden"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+
+                                    <h4 class="private">Forenposts</h4>
+                                    <input class="radioButton" name="posts"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="posts"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <input class="radioButton" name="posts"type="radio" checked data-toggle="toggle" data-style="ios">
+                                    <div class="greenLine"></div>
+                                </div>
+                            </div>
+
+
+
+                                </div>
+                            </div>
+
+                </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: "Settings",
+        data: function() {
+            return {
+                profileUser: null,              // object for user
+            }
+        },
+        mounted: function (){
+
+            this.$http.get('/api/profile/' + this.$route.params.username)
+                .then(response => {
+                    this.profileUser = response.data;
+        })
+                .catch(error => {
+                    //alert(error);
+                    this.getError(error);
+                });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -4,16 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\User;
 
 class SettingsController extends AbstractController
 {
     /**
-     * @Route("/settings", name="settings")
+     * @Route("api/settings/{username}", name="settings")
      */
-    public function index()
-    {
-        return $this->render('settings/index.html.twig', [
-            'controller_name' => 'SettingsController',
-        ]);
+    public function getUser($username) : JsonResponse {
+
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy(['username' => $username]);
+
+        if (!$user) {
+            return new JsonResponse([], Response::HTTP_NOT_FOUND);
+        }
+
+        return new JsonResponse($user->toAssoc(), Response::HTTP_OK);
     }
 }
