e7b8ce504761f32988b20ac47d6f73b0b9f0a7df
Sarah Roupec <roupecs@yahoo.com>
2020-06-14 18:02:01 +0200

    watering

--- assets/js/components/pages/Garden.vue
+++ assets/js/components/pages/Garden.vue
@@ -1,11 +1,11 @@
 <template>
 
-    <div class="row">
+    <div class="row" v-if="garden !== null">
 
         <div class="col right marginLeftRight">
 
             <div class="row garden">
-                <h1 class="col-sm">Mein Garten</h1>
+                <h1 class="col-sm">Mein Garten<span class="fontLight edit"> edit</span></h1>
                 <div class="col-sm text-right add"><span @click="add = true" class="iconGarden">
 
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 455.4 455.4" style="enable-background:new 0 0 455.4 455.4;" xml:space="preserve"><path class="path1" d="M405.5,412.8c-69.7,56.9-287.3,56.9-355.6,0c-69.7-56.9-62.6-300.1,0-364.1s293-64,355.6,0S475.2,355.9,405.5,412.8z"/><path class="path2" d="M362.8,227.9c0,14.2-11.4,25.6-25.6,25.6h-85.3v85.3c0,14.2-11.4,25.6-25.6,25.6s-25.6-11.4-25.6-25.6v-85.3
@@ -33,13 +33,13 @@
 C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
                                    </span> {{ userplant.location }}</p>
                                             <p><span class="calendar"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 455.4 455.4" style="enable-background:new 0 0 455.4 455.4;" xml:space="preserve"><path class="path1" d="m80 320h53.332031c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.332031c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m80 448h53.332031c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.332031c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m229.332031 320h53.335938c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.335938c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m229.332031 448h53.335938c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.335938c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m378.667969 320h53.332031c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.332031c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m448 64h-21.332031v-42.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031h-21.332031c-11.777344 0-21.332031 9.558594-21.332031 21.332031v42.667969h-213.335938v-42.667969c0-11.773437-9.554687-21.332031-21.332031-21.332031h-21.332031c-11.777344 0-21.335938 9.558594-21.335938 21.332031v42.667969h-21.332031c-35.285156 0-64 28.714844-64 64v320c0 35.285156 28.714844 64 64 64h384c35.285156 0 64-28.714844 64-64v-320c0-35.285156-28.714844-64-64-64zm21.332031 384c0 11.753906-9.578125 21.332031-21.332031 21.332031h-384c-11.753906 0-21.332031-9.578125-21.332031-21.332031v-233.8125h426.664062zm0 0"/></svg>
-                                   </span> Gießzeit</p>
+                                   </span> {{wateringTime(userplant.dateWatered)}}</p>
                                             <p><span class="iconGarden"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 455.4 455.4" style="enable-background:new 0 0 455.4 455.4;" xml:space="preserve"><path class="path1" d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
 		c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
 		c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z
 C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
                                    </span> Menge</p>
-                                            <button class="buttonDarkGreenSmall"> Gießen</button>
+                                            <button class="buttonDarkGreenSmall water" @click="water(userplant.dateWatered, userplant)"> Gießen</button>
                                         </div>
 
                                         <img class="col-sm-5 imgTest" v-bind:src="'../' + userplant.plant.icon"  alt="Picture of plant" height="190" >
@@ -68,13 +68,13 @@ C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
 C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
                                    </span> {{ userplant.location }}</p>
                                             <p><span class="calendar"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 455.4 455.4" style="enable-background:new 0 0 455.4 455.4;" xml:space="preserve"><path class="path1" d="m80 320h53.332031c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.332031c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m80 448h53.332031c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.332031c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m229.332031 320h53.335938c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.335938c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m229.332031 448h53.335938c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.335938c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m378.667969 320h53.332031c8.832031 0 16-7.167969 16-16v-53.332031c0-8.832031-7.167969-16-16-16h-53.332031c-8.832031 0-16 7.167969-16 16v53.332031c0 8.832031 7.167969 16 16 16zm0 0"/><path class="path1" d="m448 64h-21.332031v-42.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031h-21.332031c-11.777344 0-21.332031 9.558594-21.332031 21.332031v42.667969h-213.335938v-42.667969c0-11.773437-9.554687-21.332031-21.332031-21.332031h-21.332031c-11.777344 0-21.335938 9.558594-21.335938 21.332031v42.667969h-21.332031c-35.285156 0-64 28.714844-64 64v320c0 35.285156 28.714844 64 64 64h384c35.285156 0 64-28.714844 64-64v-320c0-35.285156-28.714844-64-64-64zm21.332031 384c0 11.753906-9.578125 21.332031-21.332031 21.332031h-384c-11.753906 0-21.332031-9.578125-21.332031-21.332031v-233.8125h426.664062zm0 0"/></svg>
-                                   </span> Gießzeit</p>
+                                   </span> {{wateringTime(userplant.dateWatered)}}</p>
                                             <p><span class="iconGarden"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 455.4 455.4" style="enable-background:new 0 0 455.4 455.4;" xml:space="preserve"><path class="path1" d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
 		c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
 		c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z
 C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
                                    </span> Menge</p>
-                                            <button class="buttonDarkGreenSmall"> Gießen</button>
+                                            <button class="buttonDarkGreenSmall" @click="water(userplant.dateWatered, userplant)"> Gießen</button>
                                         </div>
 
                                         <img class="col-sm-5 imgTest" v-bind:src="'../' + userplant.plant.icon"  alt="Picture of plant" height="190" >
@@ -176,7 +176,8 @@ C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
                plants: null,
                image: '',
                add: false,
-               search: ''
+               search: '',
+               userplantTemp: null
            }
        },
        mounted: function(){
@@ -209,7 +210,7 @@ C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
                        //|| plant.alternativeName.toLowerCase().match(this.search.toLowerCase()) can be null
                });
 
-           }
+           },
 
        },
 
@@ -223,7 +224,49 @@ C351.4,202.3,362.8,213.7,362.8,227.9z"/></svg>
 
            newUserplant: function(){
                location.reload(true);
+           },
+           
+           water: function (e, wateringDate, userplant) {
+               e.preventDefault();
+
+               // current date plus one week
+               let watering = new Date(wateringDate['date']);
+               watering.setDay(watering.getDay() + 7);
+
+               this.$http.put('/api/garden/setWateringDate/' + userplant.id, watering)
+                   .then(response => {
+                       this.userplantTemp = response.data;
+                   })
+                   .catch(error => {
+                       alert(error);
+                   });
+
+           },
+
+           wateringTime: function (wateringDate) {
+
+               let today = new Date();
+               let watering = new Date(wateringDate['date']);
+
+               let weekday = new Array(7);
+               weekday[0] = "Sonntag";
+               weekday[1] = "Montag";
+               weekday[2] = "Dienstag";
+               weekday[3] = "Mittwoch";
+               weekday[4] = "Donnerstag";
+               weekday[5] = "Freitag";
+               weekday[6] = "Samstag";
+
+               if(watering.getDate() === today.getDate() &&
+                   watering.getMonth() === today.getMonth() &&
+                   watering.getFullYear() === today.getFullYear()) {
+                   return 'Heute';
+               } else {
+                   return weekday[watering.getDay()];
+               }
+
            }
+
 
        }
 
